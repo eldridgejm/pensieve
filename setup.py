b2db5fc3c19fd12b9eab8e0808b069c1f88e9e14
Justin Eldridge <eldridgejm@gmail.com>
2020-08-24 22:28:45 -0700

    Find all packacges in setup.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="pensieve",
     version="0.2.0",
-    packages=["pensieve"],
+    packages=find_packages(),
     install_requires=["pyyaml", "requests"],
     entry_points={"console_scripts": ["pensieve = pensieve.cli:main"]},
 )
