8119b626dc437fe9fe56d0cf0d8275c8d29451e9
Justin Eldridge <eldridgejm@gmail.com>
2020-08-24 22:08:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pensieve",
-    version="0.1.0",
+    version="0.2.0",
     packages=["pensieve"],
     install_requires=["pyyaml", "requests"],
     entry_points={"console_scripts": ["pensieve = pensieve.cli:main"]},
