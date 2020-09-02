b4e6e43fdd7ba7ed6c36bab4b80741786cb8404e
Justin Eldridge <eldridgejm@gmail.com>
2020-09-02 13:35:04 -0700

    Reorganized CLI; use fzf to clone.

--- pensieve/cli.py
+++ pensieve/cli.py
@@ -1,20 +1,31 @@
 """Command line interface."""
 
 import argparse
-import datetime
+import collections
 import inspect
+import io
+import json
 import os
 import pathlib
+import shutil
+import subprocess
 import sys
 import textwrap
 
 import yaml
 
-from . import exceptions, settings, dotfile, clients
+from . import exceptions, settings, dotfile
+from .clients import GitHubClient
+
+
+HAS_FZF = bool(shutil.which('fzf'))
 
 
 _, COLUMNS = os.popen("stty size", "r").read().split()
 COLUMNS = int(COLUMNS)
+
+
+CACHE_FILENAME = ".cache.json"
 
 
 def colorizer(wrapped):
@@ -67,52 +78,67 @@ def fatal_error(msg, code=1):
     sys.exit(code)
 
 
-def valid_store(clients):
-    """Creates a function which checks an argparse argument against valid stores."""
+def parse_repository_locator(locator_string, clients):
+    try:
+        store, rest = locator_string.split(":")
+    except Exception:
+        raise ValueError("Must include store name.")
+
+    if store not in clients:
+        raise ValueError(f"{store} not a valid store.")
+    else:
+        client = clients[store]
+
+    if "/" in rest:
+        parts = rest.split("/")
+        user_or_org_prefix = parts[0] + "/"
+        repo_name = parts[1]
+    else:
+        user_or_org_prefix = ""
+        repo_name = rest
+
+    if isinstance(client, GitHubClient) and not user_or_org_prefix:
+        user_or_org_prefix = client.user + "/"
+
+    full_name = user_or_org_prefix + repo_name
+
+    Location = collections.namedtuple(
+        "RepoPath",
+        ["store_name", "client", "user_or_org_prefix", "repo_name", "full_name"],
+    )
+    return Location(store, client, user_or_org_prefix, repo_name, full_name)
+
+
+def RepositoryLocator(clients):
+    """Creates an argument checker for a repository locator string.
+
+    A repo locator string looks like this:
+        store:user_or_org_prefix/repo_name
+    or
+        store:repo_name
+    when the user_or_org_prefix is irrelevant or can be inferred.
+    """
 
     def checker(value):
-        if value not in clients:
-            raise argparse.ArgumentTypeError(f"{value} not a valid store.")
-        else:
-            return clients[value]
+        try:
+            return parse_repository_locator(value, clients)
+        except ValueError as exc:
+            raise argparse.ArgumentTypeError(str(exc))
 
     return checker
 
 
 def configure_new_parser(subparsers, clients):
     new_parser = subparsers.add_parser("new")
-    new_parser.add_argument(
-        "store", type=valid_store(clients), help="Name of the store."
-    )
     help_msg = """
-        Repository name. In the case of a GitHub store, this can either be a full
+        Repository locator. Format: <store_name>:<full_repo_name>.
+        In the case of a GitHub store, "full_repo_name" can either be a full
         repository path, like eldridgejm/pensieve, or simply a repository name,
         like pensieve. In the latter case, the new repository is assumed to be a
         user repository under the authenticated user's account.
     """
-    new_parser.add_argument("repository_name", help=help_msg)
-    new_parser.add_argument(
-        "--date",
-        action="store_true",
-        help="Prepend a date string to the repo name. Useful for 'one-off' repos.",
-    )
+    new_parser.add_argument("locator", type=RepositoryLocator(clients), help=help_msg)
     new_parser.set_defaults(cmd=cmd_new)
-
-
-def _parse_github_repository_name(possibly_full_name, default_user):
-    """
-    Takes a string like <org_or_user>/<repo_name> or just <repo_name> and
-    returns a "<org_or_user>/" prefix and the repo name.
-    """
-    if "/" in possibly_full_name:
-        parts = possibly_full_name.split("/")
-        user_or_org_prefix = parts[0] + "/"
-        repo_name = parts[1]
-    else:
-        user_or_org_prefix = default_user + "/"
-        repo_name = possibly_full_name
-
-    return user_or_org_prefix, repo_name
 
 
 def cmd_new(args):
@@ -121,60 +147,54 @@ def cmd_new(args):
     # user may omit this information, in which case it is assumed that the repo
     # will be a user repository instead of an organization repo, and the user is
     # inferred to be the authenticated user used to configure the store.
-    #
-    # we will construct the full_name from two parts: a user/org prefix and a
-    # repo name. Only in the case of a github repo is the user/org prefix non-empty
-    # the repo name is the repo name without user/org pieces, so that we can
-    # more easily add a date prefix later on.
-    if isinstance(args.store, clients.GitHubClient):
-        user_or_org_prefix, repo_name = _parse_github_repository_name(
-            args.repository_name, default_user=args.store.user
-        )
-    else:
-        user_or_org_prefix = ""
-        repo_name = args.repository_name
+    args.locator.client.new(args.locator.full_name)
 
-    # add a date prefix, if necessary
-    if args.date:
-        today = datetime.date.today()
-        prefix = today.strftime(settings.DATE_FORMAT)
-        repo_name = settings.HIGHLIGHT + prefix + settings.HIGHLIGHT + repo_name
-
-    full_name = user_or_org_prefix + repo_name
-
-    args.store.new(full_name)
-
-    print(f'New repository "{full_name}" created.')
-    args.store.clone(full_name, pathlib.Path.cwd())
+    print(f'New repository "{args.locator.store_name}:{args.locator.full_name}" created.')
+    args.locator.client.clone(args.locator.full_name, pathlib.Path.cwd())
 
 
 def configure_clone_parser(subparsers, clients):
+    if HAS_FZF:
+        nargs = '?'
+    else:
+        nargs = 1
+
     clone_parser = subparsers.add_parser("clone")
     clone_parser.add_argument(
-        "store", type=valid_store(clients), help="The store to clone from."
+        "locator",
+        type=RepositoryLocator(clients),
+        help="The store to clone from.",
+        nargs=nargs
     )
     help_msg = """
-        Repository name. In the case of a GitHub store, this can either be a full
+        Repository locator. Format: <store_name>:<full_repo_name>.
+        In the case of a GitHub store, "full_repo_name" can either be a full
         repository path, like eldridgejm/pensieve, or simply a repository name,
         like pensieve. In the latter case, the new repository is assumed to be a
         user repository under the authenticated user's account.
     """
-    clone_parser.add_argument("repository_name", help=help_msg)
     clone_parser.set_defaults(cmd=cmd_clone)
 
 
+def _fzf_select_repo():
+    cache = _read_cache()
+    names = _cached_names(cache)
+
+    result = subprocess.run('fzf', input='\n'.join(names).encode(), stdout=subprocess.PIPE)
+    return result.stdout.decode().strip()
+
+
 def cmd_clone(args):
-    # if working with a GitHub repo and a user/org is not specified, infer it
-    # to be the authenticated user's name
-    if isinstance(args.store, clients.GitHubClient) and "/" not in args.repository_name:
-        args.repository_name = args.store.user + "/" + args.repository_name
+    if args.locator is None:
+        selection = _fzf_select_repo()
+        args.locator = parse_repository_locator(selection, args.clients)
 
     try:
-        args.store.clone(args.repository_name, args.cwd)
+        args.locator.client.clone(args.locator.full_name, args.cwd)
     except exceptions.ClientError as exc:
         fatal_error(str(exc))
     else:
-        print(good(f'Cloned repository "{args.repository_name}".'))
+        print(good(f'Cloned repository "{args.locator.store_name}:{args.locator.full_name}".'))
 
 
 def configure_list_parser(subparsers, clients):
@@ -191,11 +211,36 @@ def _format_meta(msg, level=0, spacer="    "):
     return msg
 
 
+def _update_cache(store, repos_on_store):
+    """Update the cache file with information about the repos on the store.
+
+    Creates a JSON file named `CACHE_FILENAME` in the pensieve directory. The
+    file is a JSON dict whose keys are the store names. The value is a list
+    of dictionaries, one for each repo, each dictionary containing attributes
+    `name`, `topics`, and `description`.
+
+    """
+    cache_path = pathlib.Path.cwd() / CACHE_FILENAME
+
+    try:
+        with cache_path.open() as fileobj:
+            cache = json.load(fileobj)
+    except FileNotFoundError:
+        cache = {}
+
+    cache[store] = [r._asdict() for r in repos_on_store]
+
+    with cache_path.open("w") as fileobj:
+        json.dump(cache, fileobj)
+
+
 def cmd_list(args):
     for store in sorted(args.clients):
         client = args.clients[store]
 
         repos_on_store = client.list()
+        _update_cache(store, repos_on_store)
+
         for repo in sorted(repos_on_store):
             topics = ", ".join(repo.topics)
             print(highlight(repo.name) + faded(f" :: {store}"))
@@ -214,6 +259,63 @@ def cmd_list(args):
                 )
 
 
+def configure_cached_parser(subparsers, clients):
+    cached_parser = subparsers.add_parser("cached")
+    cached_parser.add_argument("what", choices={"stores", "topics", "names"})
+    cached_parser.set_defaults(cmd=cmd_cached)
+
+
+def _read_cache():
+    cache_path = pathlib.Path.cwd() / CACHE_FILENAME
+
+    try:
+        with cache_path.open() as fileobj:
+            cache = json.load(fileobj)
+    except FileNotFoundError:
+        cache = {}
+
+    return cache
+
+
+def _cached_topics(cache):
+    """Extract the set of topics from the cache."""
+    topics = set()
+    for store, repos in cache.items():
+        for repo in repos:
+            topics.update(repo["topics"])
+    return topics
+
+
+def _cached_names(cache):
+    """
+    Extract all repo names from the cache.
+
+    Format: <store_name>:<full_repo_name>, so that the repo can be cloned by
+    writing pensieve clone <store_name> <full_repo_name>.
+    """
+    names = []
+    for store, repos in cache.items():
+        for repo in repos:
+            name = store + ":" + repo["name"]
+            names.append(name)
+    return names
+
+
+def cmd_cached(args):
+    """Print info from the cache for usage in other scripts."""
+    cache = _read_cache()
+    if args.what == "stores":
+        print("\n".join(cache.keys()))
+    elif args.what == "topics":
+        topics = _cached_topics(cache)
+        print("\n".join(topics))
+    elif args.what == "names":
+        names = _cached_names(cache)
+        print("\n".join(names))
+
+    sys.exit()
+
+
 def main():
     try:
         with (pathlib.Path.cwd() / settings.DOTFILE).open() as fileobj:
@@ -230,6 +332,7 @@ def main():
     configure_clone_parser(subparsers, clients)
     configure_list_parser(subparsers, clients)
     configure_new_parser(subparsers, clients)
+    configure_cached_parser(subparsers, clients)
 
     args = parser.parse_args()
 
